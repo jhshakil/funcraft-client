6b60ca767d4004d0b7a6669b02f638558e67c62f
jhshakil <shakil11271@gmail.com>
2024-12-29 02:20:05 +0600

    add newsletter

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,11 @@
 import CategorySection from "@/components/home/CategorySection";
 import HeroSection from "@/components/home/HeroSection";
 import HomeProductSection from "@/components/home/HomeProductSection";
+import NewsLetter from "@/components/shared/NewsLetter";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { getAllCategory } from "@/services/CategoryService";
 import { getAllProduct } from "@/services/ProductService";
+import { Images } from "lucide-react";
 
 type Props = {
   searchParams: { sortBy?: string; sortOrder?: string; page?: string };
@@ -21,6 +24,51 @@ const Page = async ({ searchParams }: Props) => {
       <HeroSection />
       <CategorySection categories={categories.data} />
       <HomeProductSection products={products.data} />
+      <div className="py-[40px] px-[60px] flex justify-between items-center gap-20 border-t border-b border-border">
+        <div className="flex  items-center gap-3">
+          <Avatar className="h-[66px] w-[66px] rounded-none">
+            <AvatarImage src={"/images/shipping.png"} alt="Category" />
+            <AvatarFallback className="rounded-none">
+              <Images />
+            </AvatarFallback>
+          </Avatar>
+          <div>
+            <h3 className="text-lg capitalize">Free Shipping Orders $260+</h3>
+            <p className="text-sm text-muted-foreground">
+              A delivery service you can depend on
+            </p>
+          </div>
+        </div>
+        <div className="flex  items-center gap-3">
+          <Avatar className="h-[66px] w-[66px] rounded-none">
+            <AvatarImage src={"/images/customer support.png"} alt="Category" />
+            <AvatarFallback className="rounded-none">
+              <Images />
+            </AvatarFallback>
+          </Avatar>
+          <div>
+            <h3 className="text-lg capitalize">Customer Support</h3>
+            <p className="text-sm text-muted-foreground">
+              Satisfied customers are our best ads
+            </p>
+          </div>
+        </div>
+        <div className="flex  items-center gap-3">
+          <Avatar className="h-[66px] w-[66px] rounded-none">
+            <AvatarImage src={"/images/secure payment.png"} alt="Category" />
+            <AvatarFallback className="rounded-none">
+              <Images />
+            </AvatarFallback>
+          </Avatar>
+          <div>
+            <h3 className="text-lg capitalize">100% Secure Payments</h3>
+            <p className="text-sm text-muted-foreground">
+              The highest level of security
+            </p>
+          </div>
+        </div>
+      </div>
+      <NewsLetter />
     </div>
   );
 };
---
+++ src/components/shared/NewsLetter.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { toast } from "sonner";
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+});
+
+const NewsLetter = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setIsLoading(false);
+    toast.success(`You've successfully subscribed with ${values.email}`);
+
+    form.reset();
+  }
+
+  return (
+    <div className="pb-[64px] grid grid-cols-2 gap-20">
+      <div>
+        <small className="text-sm text-primary font-medium">
+          Join our newsletter
+        </small>
+        <h3 className="mt-2 text-3xl font-medium">
+          Get our emails for info on new items, <br /> sales and more.
+        </h3>
+        <p className="mt-2 text-muted-foreground">{`We'll email you a voucher worth $10 off your first order over $50.`}</p>
+      </div>
+      <div className="w-full max-w-sm mx-auto">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Enter your email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full" disabled={isLoading}>
+              {isLoading ? "Subscribing..." : "Subscribe to newsletter"}
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default NewsLetter;
