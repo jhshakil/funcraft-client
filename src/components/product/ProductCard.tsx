a383ff413a07a3abf21d96dfa223d2bffc75f48c
Jahid Hasan <info.jhshakil@gmail.com>
2025-03-10 03:59:40 +0600

    update product card

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -34,28 +34,29 @@ export function ProductCard({ product }: Props) {
             />
           )}
         </div>
-        <div className="p-4 space-y-2">
-          <h3 className="font-medium text-base text-gray-900">{name}</h3>
-
-          <div className="flex flex-wrap justify-between items-center">
-            <div className="flex items-center gap-2">
-              <span className="text-sm font-bold">
-                ${discountedPrice ? discountedPrice.toFixed(2) : ""}
-              </span>
-              {(discount as number) > 0 && (
-                <span className="text-sm text-gray-500 line-through">
-                  ${price ? price?.toFixed(2) : ""}
-                </span>
-              )}
-            </div>
-
-            <AddToCart
-              product={product}
-              className="bg-transparent shadow-none text-primary hover:text-white"
-            />
-          </div>
-        </div>
       </Link>
+      <div className="p-4 space-y-2">
+        <Link href={`/product/${product?.id}`}>
+          <h3 className="font-medium text-base text-gray-900">{name}</h3>
+        </Link>
+        <div className="flex flex-wrap justify-between items-center">
+          <div className="flex items-center gap-2">
+            <span className="text-sm font-bold">
+              ${discountedPrice ? discountedPrice.toFixed(2) : ""}
+            </span>
+            {(discount as number) > 0 && (
+              <span className="text-sm text-gray-500 line-through">
+                ${price ? price?.toFixed(2) : ""}
+              </span>
+            )}
+          </div>
+
+          <AddToCart
+            product={product}
+            className="bg-transparent shadow-none text-primary hover:text-white"
+          />
+        </div>
+      </div>
     </div>
   );
 }
